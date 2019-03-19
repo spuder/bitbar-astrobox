205f4c4b15e91d9060b98479db9fbf40270c7fef
Spencer Owen <owenspencer@gmail.com>
2019-03-18 20:53:25 -0600

    Check if astrobox available

--- astrobox.15s.sh
+++ astrobox.15s.sh
@@ -9,18 +9,18 @@
 
 # TODO: change to relative path that also works with symlinks
 . /Users/spencer.owen/Code/bitbar-astrobox/astrobox.config
-if [ -z "${ASTROBOX_APIKEY+x}" ]; then
+if [[ -z "${ASTROBOX_APIKEY+x}" ]]; then
   echo "ASTROBOX_APIKEY is unset, please edit astrobox.config"
   exit 1
 fi
-if [ -z "${ASTROBOX_ENDPOINT+x}" ]; then
+if [[ -z "${ASTROBOX_ENDPOINT+x}" ]]; then
   echo "ASTROBOX_ENDPOINT is unset, please edit astrobox.config"
   exit 1
 fi
 
 JQ=/usr/local/bin/jq
 
-if [ ! -e $JQ ]; then
+if [[ ! -e $JQ ]]; then
   echo "please install 'jq' first"
   exit 1
 fi
@@ -29,9 +29,13 @@ DEBUG=false
 HEADER="X-Api-Key:$ASTROBOX_APIKEY"
 
 # Request
-function runapi {
-  curl -s -gH "$HEADER" "$ASTROBOX_ENDPOINT/api/$1"
-	return 0
+# function runapi {
+#   curl -s -gH "$HEADER" "$ASTROBOX_ENDPOINT/api/$1"
+# 	return 0
+# }
+function getastrobox {
+  astrobox_available=$(curl -s -I $ASTROBOX_ENDPOINT)
+  return 0
 }
 
 # Get current state
@@ -47,22 +51,29 @@ function gettool {
   tool=$(curl -s -X GET -H "${HEADER}" "${ASTROBOX_ENDPOINT}/api/printer/tool")
   return 0
 }
-getprinter
+
+getastrobox
 
 # Ready
-if [ $(echo "$printer" | $JQ .state.flags.ready -r) = true ]; then
-  echo "Ready | color=green"
-elif [ $(echo "$printer" | $JQ .state.flags.heatingUp -r) = true ]; then
-  getbed
-  bedtarget=$(echo "$bed" | $JQ .bed.target -r)
-  bedactual=$(echo "$bed" | $JQ .bed.actual -r)
-  gettool
-  tooltarget=$(echo "$tool" | $JQ .tool0.target -r)
-  toolactual=$(echo "$tool" | $JQ .tool0.actual -r)
-  echo "Heating | color=orange"
-  echo "hotend:$toolactual/$tooltarget°C  bed:$bedactual/$bedtarget°C | color=black"
-elif [ $(echo "$printer" | $JQ .state.flags.printing -r) = true ]; then
-  echo "Printing | color=red"
+if [[ -z $astrobox_available ]]; then
+  echo "No astrobox"
 else
-  echo "No printer | color=red"
+  getprinter
+  if [[ $(echo "$printer" | $JQ .state.flags.ready -r) = true ]]; then
+    echo "Ready | color=green"
+  elif [[ $(echo "$printer" | $JQ .state.flags.heatingUp -r) = true ]]; then
+    getbed
+    bedtarget=$(echo "$bed" | $JQ .bed.target -r)
+    bedactual=$(echo "$bed" | $JQ .bed.actual -r)
+    gettool
+    tooltarget=$(echo "$tool" | $JQ .tool0.target -r)
+    toolactual=$(echo "$tool" | $JQ .tool0.actual -r)
+    echo "Heating | color=orange"
+    echo "hotend:$toolactual/$tooltarget°C  bed:$bedactual/$bedtarget°C | color=black"
+  elif [[ $(echo "$printer" | $JQ .state.flags.printing -r) = true ]]; then
+    echo "Printing | color=red"
+  else
+    echo "No printer | color=white"
+  fi
 fi
+
